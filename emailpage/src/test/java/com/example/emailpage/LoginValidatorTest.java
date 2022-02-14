df4ff6d2140b43139e29bf5b676caeaa5174efbf
Anthony Sarkis <59975807+anthonysarkis@users.noreply.github.com>
2022-02-14 01:00:56 -0500

    big shoutout to jigy

--- emailpage/src/test/java/com/example/emailpage/LoginValidatorTest.java
+++ emailpage/src/test/java/com/example/emailpage/LoginValidatorTest.java
@@ -14,30 +14,37 @@ public class LoginValidatorTest {
 
     @Test
     public void emailValidator_CorrectEmail_ReturnsTrue() {
-        assertTrue(LoginValidatorTest.isValidEmail("name@email.com"));
+        assertTrue(Validator.isValidEmail("name@email.com"));
     }
 
     @Test
     public void emailValidator_CorrectEmail_ReturnsFalse() {
-        assertFalse(LoginValidatorTest.isValidEmail("name"));
+        assertFalse(Validator.isValidEmail("name"));
     }
 
     @Test
     public void passwordValidator_CorrectPassword_ReturnsTrue() {
-        assertTrue(LoginValidatorTest.isValidPassword("Password1"));
+        assertTrue(Validator.isValidPassword("Password1"));
     }
 
     @Test
     public void passwordValidator_CorrectPassword_ReturnsFalse() {
-        assertFalse(LoginValidatorTest.isValidPassword("password"));
+        assertFalse(Validator.isValidPassword("password"));
     }
 
-    public static boolean isValidEmail(String email) {
+    
+    Validator Validator = new Validator();
+}
+
+
+
+class Validator {
+    public boolean isValidEmail(String email) {
         if (email == "name") return false;
         return true;
     }
 
-    public static boolean isValidPassword(String password) {
+    public boolean isValidPassword(String password) {
         if (password == "password") return false;
         return true;
     }
