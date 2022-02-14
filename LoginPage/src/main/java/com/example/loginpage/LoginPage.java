46f32909325e77eacf31832dfbd992d8ede5e435
Anthony Sarkis <59975807+anthonysarkis@users.noreply.github.com>
2022-02-14 00:56:05 -0500

    removed kaboum

--- LoginPage/src/main/java/com/example/loginpage/LoginPage.java
+++
@@ -1,5 +0,0 @@
-package com.example.loginpage;
-
-public class LoginPage {
-
-}
--- emailpage/src/test/java/com/example/emailpage/LoginValidatorTest.java
+++ emailpage/src/test/java/com/example/emailpage/LoginValidatorTest.java
@@ -2,7 +2,8 @@ package com.example.emailpage;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
